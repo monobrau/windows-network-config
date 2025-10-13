8ebd3757bdd78ad55ec60e31f100997adb529198
Monobrau <cknospe@gmail.com>
2025-10-13 11:39:15 -0500

    Fix Get-NetAdapterHardwareInfo parameter issue
    - Fixed incorrect -InterfaceIndex parameter usage
    - Changed to use Where-Object filter instead
    - Resolves PowerShell parameter error in physical adapter detection

--- Set-DHCP.ps1
+++ Set-DHCP.ps1
@@ -48,7 +48,7 @@ function Get-PhysicalAdapters {
         if ($adapter.Status -ne "Up") { continue }
         
         # Get adapter details
-        $adapterDetails = Get-NetAdapterHardwareInfo -InterfaceIndex $adapter.InterfaceIndex -ErrorAction SilentlyContinue
+        $adapterDetails = Get-NetAdapterHardwareInfo | Where-Object { $_.InterfaceIndex -eq $adapter.InterfaceIndex } -ErrorAction SilentlyContinue
         
         # Check if it's a physical adapter by examining various properties
         $isPhysical = $false
